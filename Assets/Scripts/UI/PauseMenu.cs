d978e6ba2e905dd7d8ad9ce50c08c7a986a38f40
Ceyrical <ashleymendez0315@gmail.com>
2024-09-23 00:30:22 -0400

    Title-Pause Menu UI
    In-game Pause Menu
    	Resume & Retry buttons
    	Title Screen with Play Button
    Scenes added - Main Screen, Game Mode, StoryMode (some buttons are not fully functioning yet)

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] public GameObject PauseMenuPanel;
+
+    public void Pause()
+    {
+        PauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        PauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Level01");
+    }
+
+}
