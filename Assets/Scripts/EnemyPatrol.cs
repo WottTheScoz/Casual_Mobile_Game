4f3cf3ff76335422d3bb78fb9e4bb122349297a9
MFORA2025 <mikeforaker592001@gmail.com>
2024-09-30 13:12:51 -0400

    Added an Enemy Patrol Script and a How to Play UI scene
    Added a script that has an object move back and forth between waypoints and added a UI scene that shows controls and objectives

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject[] patrolPoints;
+    int current = 0;
+    float rotSpeed;
+    public float speed;
+    float pointRadius = 1;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Vector2.Distance(patrolPoints[current].transform.position, transform.position) < pointRadius))
+            {
+            current++;
+            if(current >= patrolPoints.Length)
+            {
+                current = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/ShooterMechanic.cs
+++ Assets/Scripts/ShooterMechanic.cs
@@ -40,9 +40,4 @@ public class ShooterMechanic : MonoBehaviour
     {
         Destroy(bullet.gameObject);
     }
-        
-    void OnCollisionEnter(Collision other)
-    {
-        Destroy(bullet.gameObject);
-    }
 }
