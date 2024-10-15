9f0ee811c191a20e43fe0c29d452ff177205874d
Saiah0412 <97563479+Saiah0412@users.noreply.github.com>
2024-10-14 22:25:30 -0400

    Enemy push

--- Assets/Scripts/ShooterMechanic.cs
+++ Assets/Scripts/ShooterMechanic.cs
@@ -42,10 +42,6 @@ public class ShooterMechanic : MonoBehaviour
         if(other.gameObject.tag != "Exception")
         {
             Destroy(bullet.gameObject);
-            if(other.gameObject.tag == "Enemy")
-            {
-                Destroy(other.gameObject);
-            }
         }
     }
 }
